18fb9a12129cf47f21df0b3f2abf1ac4c67ead65
ci0cc0lat0 <anthonymciocco@gmail.com>
2024-02-20 16:10:40 -0600

    added .gitignore, minor changes to -h flag

--- main.cpp
+++ main.cpp
@@ -22,6 +22,8 @@ int main(int argc, char *argv[]){
         if(strcmp(argv[1],"-h") == 0){
             cerr << "A tool for recursivly searching a path for all desired filetypes\n" << endl;
             cerr << "Usage: " << argv[0] << " -flags <path> <filetype>" << std::endl;
+            cerr << "Usage example " << argv[0] << " . .pdf" << std::endl;
+
             cerr << "Flags: " << " -p"<<" for an out file" << std::endl;
             /* cerr << "Hint: Running the command without any flags and '> <filename.txt>' at the end will redirect output to that file" << endl;
             cerr << "Hint: Using . or .. as the path argument will search the current directory or the above parent directory respectively" << endl;
